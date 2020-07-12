717c400702186c83d1b93210c475c70c3e58f3d8
kaybrian <kayonbrian@gmail.com>
2020-07-12 15:21:26 +0300

    working site

--- Accounts/views.py
+++ Accounts/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import get_user_model
 from .forms import *
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 User = get_user_model()
 
@@ -14,12 +16,30 @@ def register(request):
     if form.is_valid():
         print(form.cleaned_data)
         form.save()
+        return redirect('mainsite:index')
+
     return render(request, "Accounts/register.html", context)
 
 
 def loginPage(request):
     form = LoginForm(request.POST or None)
     context = {
-        'form':form
+        'form': form
     }
-    return render(request,'Accounts/loginpage.html',context)
+    if form.is_valid():
+        email = form.cleaned_data['email']
+        password = form.cleaned_data['password']
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            messages.error(
+                request, "Sorry we couldn't Login you in, Check your Username and Password")
+            return redirect('mainsite:index')
+        else:
+            messages.info(request, "Username Or Password is Inncorrect ")
+    return render(request, 'Accounts/loginpage.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect("mainsite:index")
--- aptech/urls.py
+++ aptech/urls.py
@@ -1,16 +1,18 @@
 
 from django.contrib import admin
-from django.urls import path,include
-from django.conf import settings 
+from django.urls import path, include
+from django.conf import settings
 from django.conf.urls.static import static
 from Accounts import views as account_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('mainsite.urls')),
-    path('register/',account_views.register,name='register'),
-    path('login/',account_views.loginPage,name='loginpage'),
-] 
+    path('', include('mainsite.urls')),
+    path('register/', account_views.register, name='register'),
+    path('login/', account_views.loginPage, name='loginpage'),
+    path('logout/', account_views.logoutUser, name='logout'),
+]
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
